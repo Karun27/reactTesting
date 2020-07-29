a6494754dd3a7e95c298162de7287631ec34a5f5
Karunakar Reddy Katasani <karureddy27@gmail.com>
2020-07-29 18:38:38 -0500

    Initaial commit of components without test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
-
+import Header from './component/header/index'
+import './app.scss';
 function App() {
   return (
     <div className="App">
-      Test
+     <Header />
     </div>
   );
 }
